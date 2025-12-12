ecafea204e90f5b6c3d20ebcff055cd2b5fe5ced
ArezoMansouri <arezomansouri2000@gmail.com>
2025-12-12 13:16:21 +0430

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey[500],),
+    );
+  }
+}
+
