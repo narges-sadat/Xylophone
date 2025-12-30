d3dcdd37bfde1478681e2220284b0a90b817ecde
narges-sadat <nargessadat617@gmail.com>
2025-12-30 07:55:13 +0430

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,201 +1,96 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 
 
+
 void main() {
-  runApp(const XylophoneApp());
+  runApp(XylophoneApp());
 }
 
-class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({super.key});
+class XylophoneApp extends StatefulWidget {
 
-  void song(String Letter){
-    AssetsAudioPlayer.newPlayer().open(
-      Audio("assets/audio/$Letter.wav"),
-    );
 
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+List<String>letter=['A','B','C','D','E','F','G'];
+
+String randomletter="A";
+
+void generateRandomletter(){
+setState((){
+    randomletter=letter[Random().nextInt(letter.length)];
+});
+}
+
+// }
+  void song(String letter) {
+    if (letter == randomletter) {
+      AssetsAudioPlayer.newPlayer().open(
+        Audio("assets/audio/$letter.wav"),);
+      generateRandomletter();
+    }
   }
+
+  Widget buildmyapp(
+      {required String letter, required double width, required Color color}) {
+    return GestureDetector(
+      onTap: () {
+        song(letter);
+      },
+      child: Container(
+        height: 70,
+        width: width,
+        decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(12)
+        ),
+        child: Center(
+          child: Text(letter,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: mycolor,
-body: Center(
-  child: Column(
-    children: [
-      Container(
-        height: 70,
-        width: 300,
-        decoration: BoxDecoration(
-          color: Colors.cyan,
-          borderRadius: BorderRadius.circular(12)
-        ),
-        child: GestureDetector(
-          onTap: (){
-            song("A");
-          },
-          child: Center(
-            child: Text("A ",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-
-            ),
-            ),
+        backgroundColor: Colors.grey,
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20,),
+              Text(randomletter,style: TextStyle(fontSize: 70,color: Colors.white,fontWeight: FontWeight.bold),),
+              SizedBox(height: 20,),
+              buildmyapp(letter:"A", width:300 , color: Colors.pink),
+            SizedBox(height: 20,),
+            buildmyapp(letter: "B", width: 250, color: Colors.orange),
+            SizedBox(height: 20,),
+            buildmyapp(letter: "C", width: 200, color: Colors.green),
+            SizedBox(height: 20,),
+            buildmyapp(letter: "D", width: 170, color: Colors.blueAccent),
+            SizedBox(height: 20,),
+           buildmyapp(letter: "E", width: 150, color: Colors.yellow),
+            SizedBox(height: 20,),
+           buildmyapp(letter: "F", width: 130, color: Colors.red),
+            SizedBox(height: 20,),
+           buildmyapp(letter: "G", width: 110, color: Colors.teal),
+            ],
           ),
         ),
-      ),
-      SizedBox(height: 20,),
-      Container(
-        height: 70,
-        width: 250,
-        decoration: BoxDecoration(
-          color: Colors.purple,
-          borderRadius: BorderRadius.circular(12)
-        ),
-        child: GestureDetector(
-          onTap: (){
-            song("B");
-          },
-          child: Center(
-            child: Text("B",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-
-            ),
-            ),
-          ),
-        ),
-      ),
-      SizedBox(height: 20,),
-      Container(
-        height: 70,
-        width: 200,
-        decoration: BoxDecoration(
-          color: Colors.deepOrange,
-          borderRadius: BorderRadius.circular(12)
-        ),
-        child: GestureDetector(
-          onTap: (){
-            song("C");
-          },
-          child: Center(
-            child: Text("C",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-
-            ),
-            ),
-          ),
-        ),
-      ),
-      SizedBox(height: 20,),
-      Container(
-        height: 70,
-        width: 170,
-        decoration: BoxDecoration(
-          color: Colors.green,
-          borderRadius: BorderRadius.circular(12)
-        ),
-        child: GestureDetector(
-          onTap: (){
-            song("D");
-          },
-          child: Center(
-            child: Text("D",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-
-            ),
-            ),
-          ),
-        ),
-      ),
-      SizedBox(height: 20,),
-      Container(
-        height: 70,
-        width: 150,
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(12)
-        ),
-        child: GestureDetector(
-          onTap: (){
-            song("E");
-          },
-          child: Center(
-            child: Text("E",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-
-            ),
-            ),
-          ),
-        ),
-      ),
-      SizedBox(height: 20,),
-      Container(
-        height: 70,
-        width: 120,
-        decoration: BoxDecoration(
-          color: Colors.brown,
-          borderRadius: BorderRadius.circular(12)
-        ),
-        child: GestureDetector(
-          onTap: (){
-            song("F");
-          },
-          child: Center(
-            child: Text("F",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-
-            ),
-            ),
-          ),
-        ),
-      ),
-      SizedBox(height: 20,),
-      Container(
-        height: 70,
-        width: 100,
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(12)
-        ),
-        child: GestureDetector(
-          onTap: (){
-            song("G");
-          },
-          child: Center(
-            child: Text("G",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-
-            ),
-            ),
-          ),
-        ),
-      ),
-    ],
-  ),
-),
       ),
     );
   }
 }
-
